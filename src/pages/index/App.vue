da091ba1f69c0f2884aeeab473c3dfcafa4ca544
Dingjun Hu 胡定军 <HUDJ@cn.midea.com>
2020-12-23 11:37:53 +0800

    init

---
+++ src/pages/index/App.vue
@@ -0,0 +1,23 @@
+<template>
+	<div id="apps">
+		<img src="../../assets/logo.png" />
+	</div>
+</template>
+
+<script>
+	export default {
+		data: () => ({
+			a: 3
+		})
+	}
+</script>
+
+<style scoped>
+	#apps {
+		box-sizing: border-box;
+		width: 100%;
+		height: 100%;
+		position: relative;
+		overflow: hidden;
+	}
+</style>
---
+++ src/pages/main/App.vue
@@ -0,0 +1,26 @@
+<template>
+	<div id="apps">
+
+{{a}}
+	</div>
+</template>
+
+<script>
+
+	export default {
+		data:()=>({
+			a:3
+		})
+	}
+</script>
+
+<style scoped>
+	#apps {
+		box-sizing: border-box;
+		width: 100%;
+		height: 100%;
+		position: relative;
+		overflow: hidden;
+	}
+
+</style>
